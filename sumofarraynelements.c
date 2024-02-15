2f095b90a46edddaabb695e221df07015549a4f2
Aryan Koul <146901887+aryankoul95@users.noreply.github.com>
2024-02-15 19:28:18 +0530

    Add files via upload

---
+++ sumofarraynelements.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the size of the array");
+    scanf("%d", &n);
+    int a[n], sum = 0;
+    printf("Enter array elements");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+    printf("Sum of array elements=%d", sum);
+}

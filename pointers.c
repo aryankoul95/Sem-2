1d47ef332a4b311fb79a3594f6bb355e20f96cb3
Aryan Koul <146901887+aryankoul95@users.noreply.github.com>
2024-03-01 14:00:24 +0530

    pointers.c

--- pointers.c
+++ pointers.c
@@ -42,3 +42,19 @@
 //  }
 
 //WAP to read and display the elements of an array using pointer
+// #include<stdio.h>
+// int main()
+// {
+//     int i , arr[5];
+//     printf("Enter the Data : ");
+//     for(i=0;i<5;i++)
+//     {
+//         scanf("%d",arr+i);
+//     }
+//     printf("Array elements are :\n");
+//     for(i=0;i<5;i++)
+//     {
+//         printf("%d\t",*(arr+i));
+//     }
+//     return 0;
+// } 

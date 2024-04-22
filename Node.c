ec34bafa4e88d070467b765b8712195f01667cca
Aryan Koul <146901887+aryankoul95@users.noreply.github.com>
2024-04-22 14:14:17 +0530

    Add files via upload

---
+++ Node.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node {
+    int data ;//store value
+    struct Node *next;// reference to next Node to access other element;
+};
+int main()
+{
+    struct Node *t=(struct Node*)malloc(sizeof(struct Node));//allocating memory for a struct node in C. This line of code dynamically allocates memory using malloc for a struct named Node. After allocation, t will point to the newly allocated memory.
+    printf("Enter Data : ");// print 
+    scanf("%d",&t->data);// scan the data using pointer
+    t->next=NULL;
+    struct Node *t1 = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter data : ");
+    scanf("%d",&t1->data);
+    t1->next=NULL;
+    t->next=t1;
+    struct Node *t2 = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter data : ");
+    scanf("%d",&t2->data);
+    t2->next = NULL;
+    t1->next=t2;
+    scanf("%d->%d->%d",&t->data,&t1->data,&t2->data);
+    
+}
